01f735153a6d9e58d5600347280acdb2ffd6c541
Тимофей Мухаметзянов <ecologic17@gmail.com>
2023-11-05 23:35:09 +0500

    5.16.

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,21 @@
-function  isStringLength (str, leng)  {
-  if (str.length<=leng) return true
-  return false
+function  goesBeyondTheWorkingDay (dayStart, dayEnd, meetingStart, meetingDuration)  {
+  dayStart = dayStart.split(':');
+  if (dayStart[0][0]=='0') {
+    if (dayStart[0][1]=='0') { dayStart[0]=0; }
+    else { dayStart[0]=parseInt(dayStart[0][1]); } }
+  else { dayStart[0]=parseInt(dayStart[0]); }
+
+  dayEnd= dayEnd.split(':');
+  if (dayEnd[0][0]=='0') {
+    if (dayEnd[0][1]=='0') { dayEnd[0]=0; }
+    else { dayEnd[0]=parseInt(dayEnd[0][1]); } }
+  else { dayEnd[0]=parseInt(dayEnd[0]); }
+
+  meetingStart = meetingStart.split(':');
+  if (meetingStart[0][0]=='0') {
+    if (meetingStart[0][1]=='0') { meetingStart[0]=0; }
+    else { meetingStart[0]=parseInt(meetingStart[0][1]); } }
+  else { meetingStart[0]=parseInt(meetingStart[0]); }
 }
 
-function  isPalindrome (str)  {
-  str.trim()
-  str.toLowerCase()
-  for (let i =0; i < parseInt(str.length/2,10); i++) {
-    if (str[i]!=str[-i]) {return false}
-  }
-  return true
-}
+
