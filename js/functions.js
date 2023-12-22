e04b17dc71232b14825d18218e36f1838c625c7b
Тимофей Мухаметзянов <ecologic17@gmail.com>
2023-12-22 23:54:08 +0500

    Изменение

--- js/functions.js
+++ js/functions.js
@@ -49,9 +49,7 @@ checkNumber('ECMAScript 2022');
 function addsSimbol (string, limit, add){
   const realAdd = limit - string.length;
 
-  if(realAdd <= 0){
-    return string;
-  }
+  if(realAdd <= 0){return string;}
   return add.slice(0, realAdd % add.length) + add.repeat(realAdd / add.length) + string;
 }
 
